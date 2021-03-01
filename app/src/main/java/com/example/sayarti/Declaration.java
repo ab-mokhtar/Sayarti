c79dcb275a99ce38adf17ccc36c546aae1a3cf5d
Abbes <mokhtarabbes9@gmail.com>
2021-03-01 10:46:31 +0100

    firebase

---
+++ app/src/main/java/com/example/sayarti/Declaration.java
@@ -0,0 +1,50 @@
+package com.example.sayarti;
+
+public class Declaration {
+    private String matricule;
+    private String type_panne;
+    private String localisation;
+    private Boolean etat;
+
+    public Declaration(String matricule, String type_panne, String localisation) {
+        this.matricule = matricule;
+        this.type_panne = type_panne;
+        this.localisation = localisation;
+        this.etat = false;
+    }
+
+    public Boolean getEtat() {
+        return etat;
+    }
+
+    public void setEtat(Boolean etat) {
+        this.etat = etat;
+    }
+
+    public Declaration() {
+    }
+
+    public String getMatricule() {
+        return matricule;
+    }
+
+    public void setMatricule(String matricule) {
+        this.matricule = matricule;
+    }
+
+    public String getType_panne() {
+        return type_panne;
+    }
+
+    public void setType_panne(String type_panne) {
+        this.type_panne = type_panne;
+    }
+
+    public String getLocalisation() {
+        return localisation;
+    }
+
+    public void setLocalisation(String localisation) {
+        this.localisation = localisation;
+    }
+}
